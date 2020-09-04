8a204cdcf6c481f11d100604638c453e84213eb4
shijianan <NANNAN950517>
2020-09-04 16:36:40 +0800

    添加安装流程

--- producer/src/main/java/com/sjn/producer/component/ProducerMessage.java
+++ producer/src/main/java/com/sjn/producer/component/ProducerMessage.java
@@ -1,8 +1,5 @@
 package com.sjn.producer.component;
 
-import org.springframework.amqp.AmqpException;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,15 +13,12 @@ public class ProducerMessage {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    public void sendMsg(String msg) {
+    public void sendMsg(String msg, long time) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println("消息发送时间:" + sdf.format(new Date()));
-        rabbitTemplate.convertAndSend("test_exchange", "test_queue_1", msg, new MessagePostProcessor() {
-            @Override
-            public Message postProcessMessage(Message message) throws AmqpException {
-                message.getMessageProperties().setHeader("x-delay", 10000);
-                return message;
-            }
+        rabbitTemplate.convertAndSend("test_exchange", "test_queue_1", msg, message -> {
+            message.getMessageProperties().setHeader("x-delay", time);
+            return message;
         });
     }
 }
--- producer/src/test/java/com/sjn/producer/component/MessageServiceImplTest.java
+++ producer/src/test/java/com/sjn/producer/component/MessageServiceImplTest.java
@@ -1,15 +1,29 @@
 package com.sjn.producer.component;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @description:
  * @date: 2020/8/6
  * @author: ShiJiaNan
  */
+@SpringBootTest
 public class MessageServiceImplTest {
 
-    @org.junit.Test
-    public void sendMsg() {
+    @Autowired
+    private ProducerMessage producerMessage;
+
+    @Test
+    public void sendMsg() throws InterruptedException {
+        //先入后消费
+        producerMessage.sendMsg("hello", 20 * 1000);
+        Thread.sleep(10 * 1000);
+        //后入先消费
+        producerMessage.sendMsg("1111", 2 * 1000);
+        new CountDownLatch(1).await();
     }
 }
